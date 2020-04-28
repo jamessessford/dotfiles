bf005e6d8cf9c40f8406424589844fb4aa4e2fdd
James Sessford <hello@jamessessford.com>
2020-04-28 16:03:14 +0100

    wip

--- setup.sh
+++ setup.sh
@@ -9,4 +9,10 @@ git config --global user.name "James Sessford"
 git config --global user.email "hello@jamessessford.com"
 git config --global core.excludesFile $HOME/.dotfiles/.gitignore_global
 
+rm -rf $HOME/.config/Code/User/settings.json
+rm -rf $HOME/.config/Code/User/keybindings.json
+
+ln -s $HOME/.dotfiles/code/settings.json $HOME/.config/Code/User/settings.json
+ln -s $HOME/.dotfiles/code/keybindings.json $HOME/.config/Code/User/keybindings.json
+
 echo "Close this shell and reopen..."
