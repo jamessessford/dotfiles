e49eb094b6e81a2abe3dacc0016738ce78d049f2
James Sessford <james.sessford@preferredmanagement.co.uk>
2020-05-31 09:01:17 +0100

    wip

--- start_services.sh
+++ start_services.sh
@@ -3,11 +3,13 @@ service supervisor stop
 service php5.6-fpm stop
 service php7.4-fpm stop
 service nginx stop
+service apache stop
 service mysql stop
 
 service cron start
 service supervisor start
 service php5.6-fpm start
 service php7.4-fpm start
-service nginx start
+#service nginx start
+service apache start
 service mysql start
