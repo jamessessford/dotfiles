18664f90f210fa7f3018e450a6c31fd1a79f989f
James Sessford <hello@jamessessford.com>
2021-03-30 14:11:54 +0100

    wip

--- path.zsh
+++ path.zsh
@@ -1,11 +1,11 @@
 #   NPM Global Modules
-export PATH="/home/james/.npm-global/bin:$PATH"
+export PATH="$HOME/.npm-global/bin:$PATH"
 
 #   Composer global packages
-export PATH="/home/james/.composer/vendor/bin:$PATH"
+export PATH="$HOME/.config/composer/vendor/bin:$PATH"
 
 #   Rrvwmrrr CLI
-export PATH="/home/james/packages/rrvwmrrr-cli:$PATH"
+export PATH="$HOME/.rrvwmrrr-cli:$PATH"
 
 #   Put JAVA on the path
 export PATH="/usr/java/bin:$PATH"
