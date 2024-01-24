73b0fd9e201dcc7fe08204cde544c942f810be8d
James Sessford <james.sessford@preferredmanagement.co.uk>
2024-01-24 07:16:18 +0000

    Added Redis to start up script

--- start_services.sh
+++ start_services.sh
@@ -2,8 +2,10 @@ sudo service cron stop
 sudo service php5.6-fpm stop
 sudo service apache2 stop
 sudo service mariadb stop
+sudo service redis-server stop
 
 sudo service cron start
 sudo service php5.6-fpm start
 sudo service apache2 start
 sudo service mariadb start
+sudo service redis-server start
